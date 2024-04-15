e186127cbfe495a190d91f0a84f005ae520e9d2e
Alfred Wallace <contact@alfredwallace.me>
2024-04-15 16:26:50 -0700

    Create SVG without color typechecking, also gitignore generated icons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { writeFile } = require("fs/promises")
 const inquirer = require("inquirer");
-const snap = require("snapsvg")
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
+const { writeFile } = require("fs/promises");
 
 const questions = [
     {
@@ -8,9 +8,7 @@ const questions = [
         message: "Enter up to 3 characters:",
         type: "input",
         validate: (input) => {
-            //console.log(`3 character input: ${input}`);
             if (input.length <= 3) {
-                //console.log(`"${input}" is valid~`);
                 return true;
             }
             return "Enter UP TO 3 characters";
@@ -24,7 +22,7 @@ const questions = [
             console.log(`Color input: "${input}"`);
 
             return true;
-        } 
+        }
     },
     {
         name: "shape",
@@ -44,21 +42,52 @@ const questions = [
     }
 ];
 
-const keysToShapes = {
-    "Circle": {Circle},
-    "Triangle": {Triangle},
-    "Square": {Square}
-};
+
 
 function Init() {
     inquirer.prompt(questions)
         .then((answers) => {
             console.log(`answers: ${JSON.stringify(answers)}`);
             // We have a shape, a color, and 3 characters
-            let outputImage = Snap(300, 200);
-            //let shape = 
-         })
-        .catch((err) => { 
+            var threeCharacters = answers.threeCharacters;
+            var textColor = answers.textColor;
+            if (textColor === "") {
+                textColor = "white";
+            }
+            var shape = answers.shape;
+            var shapeColor = answers.shapeColor;
+            if (shapeColor === "") {
+                shapeColor = "black";
+            }
+
+            let shapeObject;
+            switch (shape) {
+                case "Circle":
+                    shapeObject = new Circle();
+                    break;
+                case "Square":
+                    shapeObject = new Square();
+                    break;
+                case "Triangle":
+                    shapeObject = new Triangle();
+                    break;
+                default:
+                    throw new Error("Shape failed to generated.");
+            }
+
+
+            shapeObject.setColor(shapeColor);
+            let svgOutput = `<svg width="300" height="200"><g>`;
+            svgOutput += shapeObject.render();
+            svgOutput += `<text fill="${textColor}" font-size="45" font-family="Comic Sans" x="0" y="0">${threeCharacters}</text>`;
+            svgOutput += "</g></svg>";
+
+            writeFile("icon.svg", svgOutput, (err) => {
+                if (err) throw err;
+                console.log("Successfully saved icon.svg!")
+            });
+        })
+        .catch((err) => {
             console.log(`Uh ohhh! ERROR: ${err}`)
         });
 }
